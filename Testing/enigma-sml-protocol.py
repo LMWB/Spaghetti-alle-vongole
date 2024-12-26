dd5350a7830e7389110642efb4c3200765675dbb
MaxWilli86 <lukas.bartels@smartcube360.de>
2024-12-26 20:53:24 +0100

    added readme, added python code

--- Testing/enigma-sml-protocol.py
+++ Testing/enigma-sml-protocol.py
@@ -3,16 +3,41 @@ filename = 'stromzaehlerFZS-01.log'
 def parse_line(l):
     # 'l' will be of class string
     # it must be typecast to int to make calculations and bitwise operations 
-    print(l)
-    print()
+    list_hex_string = l.split()
+    sml_package = []
+    for i in list_hex_string:
+        if(i=="HIT"):
+            print(i)
+        else:
+            sml_package.append(int(i, 16))
+    return sml_package
 
+# CCITT CRC16
+def compute_crc16(l):
+    # 'l' will be of class list with integer elements 
+    pass
 
-with open(filename) as file:
-    # 'line' will be of class str
-    for line in file:
-        #parse_line(line)
-        pass
+def is_sml_message(l):
+    # 'l' will be of class list with integer elements 
+    if( l[0] == 0x01 and l[1] == 0x01 and l[2] == 0x76 ):
+        return True
+    else:
+        return False
 
-parse_line(line)
-print(type(line))
-print(f'Hello World!')
+def decode_sml_message(l):
+    # 'l' will be of class list with integer elements 
+    pass
+
+if( __name__ == "__main__"):
+    with open(filename) as file:
+        # 'line' will be of class str
+        for line in file:
+            #parse_line(line)
+            pass
+
+    array = parse_line(line)
+
+    print(type(line))
+    print(type(array))
+    print(is_sml_message(array))
+    print(f'end of script')
